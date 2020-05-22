a9e2f8a1d67dbd110c5e496a320834f06b0b78e0
edux76 <eddux76@gmail.com>
2020-05-22 20:51:20 +0100

    upload

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\emfbr\Documents\Edu_Docs\DJANGO\django_craiglist\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
